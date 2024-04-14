35e380e7ff910b9e5439501c213219bb60801f06
Farzin ahammed <farzinahammed.in@gmail.com>
2024-04-14 17:42:01 +0530

    server connection and socket.io connection succesfully completed

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,4 +1,41 @@
 
+import express from 'express'
+import dotenv from 'dotenv';
+import http from 'http'
+import {Server} from 'socket.io'
+import cors from 'cors'
+
+dotenv.config()
+const app = express()
+app.use(cors())
+
+const port = process.env.PORT||5000
+
+const server = http.createServer(app)
+
+
+const io = new Server(server,{
+    cors:{
+        origin:"*",
+        methods: ["GET","POST"]
+    }
+})
+
+io.on('connection',(socket)=>{
+    console.log('user is connected')
+
+    socket.on("disconnect",()=>{
+        console.log("user is disconnected")
+    })
+})
+
+
+
+server.listen(port,()=>{
+    console.log(`server is running on port ${port}`)
+})
+
+
 
 
 
